1e69ea83c1999465b69be6397ba1f0e56dcee3e5
NicolaPlacentino <nyko0921@gmail.com>
2022-12-12 16:20:13 +0100

    completed exercise 1

--- script.js
+++ script.js
@@ -3,8 +3,48 @@
  Stabilire il vincitore, in base a chi fa il punteggio più alto.
  Stampiamo in pagina i due tiri e il risultato */
 
+// 1. Prendo gli elementi dal DOM
+const userThrow = document.getElementById('user-throw')
+const cpuThrow = document.getElementById('cpu-throw')
+const button = document.getElementById('button')
+const winner = document.getElementById('winner')
+
+const results = []
+
+// 2. Creo un event listener
+button.addEventListener('click', function(){
+    
+    results.shift()
+    results.pop()
+
+    // 3. Creo un ciclo che...
+    for (let i = 0; i < 2; i++) {
 
 
+        // 4. ...generi due numeri random...
+        const randomNumber = Math.floor(Math.random() * 6 + 1)
+
+        // 5. ...e li aggiunga all'array
+        results.push(randomNumber)
+        
+    }
+    
+    // 6. Stampo in pagina e annuncio il vincitore
+    const firstElement = results[0]
+    userThrow.innerText = firstElement
+
+    const lastElement = results[results.length - 1]
+    cpuThrow.innerText = lastElement
+
+    if (firstElement > lastElement){
+        winner.innerText = 'Vince il giocatore!'
+    } else if (firstElement < lastElement){
+        winner.innerText = 'Vince il computer!'
+    } else {
+        winner.innerText = 'È un pareggio!'
+    }
+
+});
 
 
 // -----------------------------------------------------------------
